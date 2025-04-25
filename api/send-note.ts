77c9749f996912ff151f93b980a9a6dd6d5df55b
OpciobyPO <colliou@opcio.fr>
2025-04-25 22:12:20 +0200

    Update send-note.ts

--- api/send-note.ts
+++ api/send-note.ts
@@ -6,8 +6,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).json({ message: 'Méthode non autorisée' });
   }
 
-  const { prenom, nom, secu, email } = req.body;
-
+const { prenom, nom, secu, email, contrat, cabinet, adresse, telephone, prix, siret, adeli } = req.body;
+  
   if (!prenom || !nom || !secu || !email) {
     return res.status(400).json({ error: 'Champs manquants' });
   }
